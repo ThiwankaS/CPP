f5aecbb5d3b099c82b6fabdb845611f1fac34ef9
Thiwanka Somachandra <thivankas@outlook.com>
2025-08-16 06:14:08 +0300

    creating more instances and testing their existance

--- M01/ex01/main.cpp
+++ M01/ex01/main.cpp
@@ -5,6 +5,27 @@
 #include "test.hpp"
 
 int main(void) {
-    std::cout << "heloooooo, bro! \n";
+
+    test("test case 01");
+    TEST_NAME("Creating 05 Zombie objects with the Name Rick Grimes");
+    LINE_DATA;
+    Zombie *zombies = zombieHorde(5,"Rick Grimes");
+    LINE_DATA;
+
+    test("test case 02");
+    TEST_NAME("announce() method call on each object in the array");
+    LINE_DATA;
+    for(int i = 0; i < 5; i++) {
+        std::cout << "zombies[" << i << "] : ";
+        (zombies + i)->announce();
+    }
+    LINE_DATA;
+
+    test("test case 03");
+    TEST_NAME("Releasing allocated memory for each object");
+    LINE_DATA;
+    delete [] zombies;
+    LINE_DATA;
+
     return (0);
 }
--- M01/ex01/utilities.hpp
+++ M01/ex01/utilities.hpp
@@ -7,6 +7,6 @@
 #include "Zombie.hpp"
 
 Zombie* newZombie(std::string name);
-void randomChump( std::string name );
+Zombie* zombieHorde( int N, std::string name );
 
 #endif
