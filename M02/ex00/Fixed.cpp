9c12e0a8ac2afa90958d0b53f1a080dad3bc43ac
Thiwanka Somachandra <thivankas@outlook.com>
2025-09-01 04:54:09 +0300

    format output message

--- M02/ex00/Fixed.cpp
+++ M02/ex00/Fixed.cpp
@@ -3,16 +3,16 @@
 const int Fixed::fp_bits = 8;
 
 Fixed::Fixed() : fp_vlaue(0) {
-    std::cout << "Default constructor called\n";
+    std::cout << "Default constructor called. \n";
 }
 
 Fixed::Fixed(const Fixed& other) {
-    std::cout << "Copy constructor called\n";
+    std::cout << "Copy constructor called. \n";
     *this = other;
 }
 
 Fixed& Fixed::operator=(const Fixed& other) {
-    std::cout << "Copy assignment operator called\n";
+    std::cout << "Copy assignment operator called. \n";
     if(this != &other) {
         this->fp_vlaue = other.getRawBits();
     }
@@ -20,15 +20,15 @@ Fixed& Fixed::operator=(const Fixed& other) {
 }
 
 Fixed::~Fixed() {
-    std::cout << "Destructor called\n";
+    std::cout << "Destructor called. \n";
 }
 
 int Fixed::getRawBits(void) const {
-    std::cout << "getRawBits member function called\n";
+    std::cout << "getRawBits member function called. \n";
     return (fp_vlaue);
 }
 
 void Fixed::setRawBits(int const raw) {
-    std::cout << "setRawBits member function called\n";
+    std::cout << "setRawBits member function called. \n";
     fp_vlaue = raw;
 }
