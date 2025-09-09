cc0dd9f8c2dca76ee85480109cee144c1ab716fb
Thiwanka Somachandra <thivankas@outlook.com>
2025-09-09 19:11:47 +0300

    starting on M05

---
+++ M05/ex00/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "Bureaucrat.hpp"
+#include "test.hpp"
+
+int main (void) {
+
+	LINE_DATA;
+	Bureaucrat b("Thiwanka", 110);
+	b.print();
+
+    return (EXIT_SUCCESS);
+}
