a18fc905bd0f799ade302754da919dff39176b2e
Thiwanka Somachandra <thivankas@outlook.com>
2025-10-21 05:47:49 +0300

    minimum possible program

---
+++ M09/ex02/PmergeMe.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class PmergeMe {
+    
+};
+
+#include "PmergeMe.tpp"
--- M09/ex02/main.cpp
+++ M09/ex02/main.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 #include "PmergeMe.hpp"
 
-
 /*
     base example
     11 2 17 0 16 8 15 6 10 3 7 1 18 9 19 14 12 5 20 4 13
 */
 
 int main (int argc, char *argv[]) {
-    
+    (void) argc;
+    (void) argv;
     return (EXIT_SUCCESS);
 }
