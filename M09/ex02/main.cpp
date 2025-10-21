7c28b434aa2bd8e3829dae533b674e39fb4a7bb2
Thiwanka Somachandra <thivankas@outlook.com>
2025-10-21 05:44:04 +0300

    restart project from scratch

--- M09/ex02/main.cpp
+++ M09/ex02/main.cpp
@@ -2,19 +2,13 @@
 #include <vector>
 #include "PmergeMe.hpp"
 
+
+/*
+    base example
+    11 2 17 0 16 8 15 6 10 3 7 1 18 9 19 14 12 5 20 4 13
+*/
+
 int main (int argc, char *argv[]) {
-
-    std::vector<int> data = {11,2,17,0,16,8,15,6,10,3,7,1,18,9,19,14,12,5,20,4,13};
-
-    if (argc > 2) {
-        (void) argv[1];
-        PmergeMe pm;
-        pm.print("data", data);
-        pm.sort_vec(data);
-        pm.print("sorted data : ", data);
-        std::cout << "Number of comparissions : "
-            << PmergeMe::number_of_comparissons
-            << std::endl;
-    }
+    
     return (EXIT_SUCCESS);
 }
